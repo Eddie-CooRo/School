dd7ed924f9e8ee26c238743790540338123ca8a4
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-19 12:47:44 +0430

    Use socket for the first time ever 😁
    Thanks to @h3isenbug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,118 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isOpen: false,
+      isConnected: false,
+      numbers: []
+    };
+
+    this.initSocket();
+  }
+
+  initSocket() {
+    const url = 'ws://0.tcp.ngrok.io:16210/ws';
+
+    this.ws = new WebSocket(url);
+
+    this.ws.onopen = () => {
+      // connection opened
+      this.setState({ isConnected: true, isOpen: true });
+
+      // this.ws.send('something'); // send a message
+    };
+
+    this.ws.onmessage = e => {
+      // a message was received
+      let newData = [...this.state.numbers];
+      newData.push({
+        key: `${Date.now()}`,
+        value: e.data
+      });
+
+      this.setState({ numbers: newData });
+    };
+
+    this.ws.onerror = e => {
+      // an error occurred
+      console.warn(`error: ${e.message}`);
+    };
+
+    this.ws.onclose = e => {
+      // connection closed
+      this.setState({ isOpen: false });
+
+      console.warn(`closing: code: ${e.code} - reason: ${e.reason}`);
+    };
+  }
+
   render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#F8F8FF'
-        }}
-      >
-        <Text
+    if (this.state.isConnected) {
+      return (
+        <View
           style={{
-            fontSize: 28,
-            color: 'black'
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#F8F8FF',
+            padding: 10
           }}
         >
-          Hello there!
-        </Text>
-      </View>
-    );
+          <FlatList
+            ref={flatList => {
+              this.flatList = flatList;
+            }}
+            style={{
+              flex: 1,
+              alignSelf: 'stretch',
+              borderWidth: 2,
+              borderRadius: 10
+            }}
+            data={this.state.numbers}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  height: 85,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'stretch',
+                  borderRadius: 10,
+                  backgroundColor: 'coral',
+                  margin: 10
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 28,
+                    color: 'white'
+                  }}
+                >
+                  {item.value}
+                </Text>
+              </View>
+            )}
+            onContentSizeChange={() => this.flatList.scrollToEnd()}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#F8F8FF'
+          }}
+        >
+          <ActivityIndicator size='large' color='coral' />
+        </View>
+      );
+    }
   }
 }
 
